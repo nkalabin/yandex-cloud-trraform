0b2744c153c8a55f2fbd0a1a554cb92929056aa6
Николай Калабин <nkalabin@ptsecurity.com>
2026-03-22 04:48:57 +0700

    test

--- backend.tf
+++ backend.tf
@@ -1,9 +1,5 @@
 terraform {
   backend "s3" {
-    endpoints = {
-      s3 = "https://storage.yandexcloud.net"
-      dynamodb = "https://docapi.serverless.yandexcloud.net/ru-central1/b1g1evjg1313adh3k2jl/etntn6v5hu9bv42ejnrj"
-    }
     bucket = "terraform-nkalabin"
     region = "ru-central1"
     key    = "terraform.tfstate"
